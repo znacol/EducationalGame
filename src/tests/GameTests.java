92c1eb19800eaf083219329deeb3a4e5aaa3052c
Travis Johnson <cjohnso0@mymail.mines.edu>
2015-04-16 11:31:29 -0600

    Unimplemented initial failing JUnit Tests added

--- src/tests/GameTests.java
+++ src/tests/GameTests.java
@@ -7,13 +7,33 @@ import org.junit.Test;
 
 public class GameTests {
 
-	@Before
-	public void setUp() throws Exception {
+	@Before	// initialize a Game with a list of targets
+	public void setUp() {
+		
 	}
 
-	@Test
-	public void test() {
+	@Test // target contain method returns expected boolean
+	public void targetContains(){
+		fail("Not yet implemented");
+	}
+	
+	@Test // that targets spawn in desired panel/area 
+	public void targetsOnScreen() {
+		fail("Not yet implemented");
+	}
+	
+	@Test	// that targets do not spawn over each other
+	public void noTargetsOverlap() {
+		fail("Not yet implemented");
+	}
+	
+	@Test	// that targets do not spawn on tank/HUD
+	public void noTargetsOnOtherComponents() {
 		fail("Not yet implemented");
 	}
 
+	@Test	// that when barrelAngle is set, it changes to appropriate angle (including the gui coords)
+	public void correctBarrelAngle() {
+		fail("Not yet implemented");
+	}
 }
